ad3a391476da7c4ad1121a11ceaba812c2d16dd3
Patrick Tchankue <ptchankue@gmail.com>
2015-08-17 15:34:15 +0200

    Updating the setup to allow pip install

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='KalturaClient',
     version='1.0.0',
     url='http://www.kaltura.com/api_v3/testme/client-libs.php',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
+    #packages=find_packages(),
     license='AGPL',
     description='A Python module for accessing the Kaltura API.',
     long_description=open('README.txt').read(),
+    install_requires = [
+            'poster',
+        ],
+    author='Patrick Tchankue',
+    keywords = ['kaltura', 'python'], # arbitrary keywords
+    url = 'https://github.com/ptchankue/KalturaGeneratedAPIClientsPython',
+    download_url = 'https://github.com/ptchankue/KalturaGeneratedAPIClientsPython/tarball/0.1',
 )
